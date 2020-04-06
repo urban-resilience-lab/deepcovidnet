7b96da5cd90bf70e5846a81ebfc50c2e56c8ca9f
Bora Oztekin <borao@users.noreply.github.com>
2020-04-05 22:18:27 -0500

    Optimize tract-level information extraction

--- covid_county_prediction/BaseCountyDataset.py
+++ covid_county_prediction/BaseCountyDataset.py
@@ -9,6 +9,7 @@ import os
 import string
 from datetime import date
 
+
 ### TODO: FIX TIMEZONES
 
 class BaseCountyDataset(Dataset, ABC):
@@ -18,10 +19,10 @@ class BaseCountyDataset(Dataset, ABC):
 
     def get_poi_info(self):
         # get county code for each poi
-        county_df = pd.read_csv(config.place_county_cbg_file, 
-                            usecols=['safegraph_place_id', 'countyFIPS'], 
-                            dtype={'countyFIPS': str}
-            )
+        county_df = pd.read_csv(config.place_county_cbg_file,
+                                usecols=['safegraph_place_id', 'countyFIPS'],
+                                dtype={'countyFIPS': str}
+                                )
         county_df = county_df.dropna().set_index('safegraph_place_id')
 
         # get top level category for each poi 
@@ -40,60 +41,52 @@ class BaseCountyDataset(Dataset, ABC):
         return final_df.to_dict(orient='index')
 
     def make_census_dict(self):
-        if __name__ == '__main__':
-            main_df = pd.DataFrame()
-            for file in glob.glob(config.sg_open_census_data_path + "cbg_*.csv"):
-                df = pd.read_csv(file, converters={
-                    'census_block_group': lambda x: str(x)})  # The converter is used to retain leading zeros
-
-                # Convert census block groups to FIPS codes and use as index named FIPS
-                df.index = df['census_block_group'].apply(lambda x: x[:5]).astype(
-                    int)  # Save only first five chars of index
-                df.drop('census_block_group', axis=1, inplace=True)
-                df.index.name = "FIPS"
-
-                # Aggregate data by FIPS codes
-                df = df.groupby("FIPS").sum()
-                main_df = main_df.join(df, how="outer")
-            return main_df.to_dict()
+        dfs = []
+        for file in glob.glob(config.sg_open_census_data_path + "cbg_*.csv"):
+            df = pd.read_csv(file, dtype={'census_block_group': str})  # The converter is used to retain leading zeros
+            dfs.append(df)
+        dfs = pd.concat(dfs, axis=1)
+        dfs.index = dfs.iloc[:, 0].str.slice(0, 5).astype(int)
+        dfs = dfs.groupby(dfs.index).sum()
+        return dfs
 
     def read_sg_patterns_monthly(self, start_date, end_date):
         pass
 
     def read_sg_patterns_monthly_file(self, csv_file):
-        df = pd.read_csv(csv_file, 
-                usecols=[
-                        'safegraph_place_id', 
-                        'date_range_start', 
-                        'date_range_end', 
-                        'raw_visit_counts',
-                        'visits_by_day'
-                        # bucketed_dwell_times may be useful to see
-                        # how long people stayed
-                    ],
-                converters={'visits_by_day': (lambda x: np.array([int(s) for s in re.split(r'[,\s]\s*', x.strip('[]'))]))}
-            )
-       
+        df = pd.read_csv(csv_file,
+                         usecols=[
+                             'safegraph_place_id',
+                             'date_range_start',
+                             'date_range_end',
+                             'raw_visit_counts',
+                             'visits_by_day'
+                             # bucketed_dwell_times may be useful to see
+                             # how long people stayed
+                         ],
+                         converters={'visits_by_day': (
+                             lambda x: np.array([int(s) for s in re.split(r'[,\s]\s*', x.strip('[]'))]))}
+                         )
+
         if (df['date_range_start'] == df.iloc[0]['date_range_start']).all() and \
-            (df['date_range_end'] == df.iloc[0]['date_range_end']).all():  
-            
+                (df['date_range_end'] == df.iloc[0]['date_range_end']).all():
             start_time = df.iloc[0]['date_range_start']
-            end_time   = df.iloc[0]['date_range_end']
+            end_time = df.iloc[0]['date_range_end']
 
             df.drop(labels=['date_range_start', 'date_range_end'], axis='columns', inplace=True)
 
-        #start_time and end_time can be processed based on model here
+        # start_time and end_time can be processed based on model here
 
         df['countyFIPS'] = df['safegraph_place_id'].apply(
-            lambda x : self.poi_info[x]['countyFIPS'] 
-                        if x in self.poi_info and self.poi_info[x]['countyFIPS'] 
-                        else '00000'
+            lambda x: self.poi_info[x]['countyFIPS']
+            if x in self.poi_info and self.poi_info[x]['countyFIPS']
+            else '00000'
         )
 
         df['top_category'] = df['safegraph_place_id'].apply(
-            lambda x : self.poi_info[x]['top_category'] 
-                        if x in self.poi_info and self.poi_info[x]['top_category'] 
-                        else 'Unknown'
+            lambda x: self.poi_info[x]['top_category']
+            if x in self.poi_info and self.poi_info[x]['top_category']
+            else 'Unknown'
         )
 
         top_cats = set()
@@ -104,14 +97,14 @@ class BaseCountyDataset(Dataset, ABC):
         num_days = len(df.iloc[0]['visits_by_day'])
 
         for cat in top_cats:
-            colname = cat.translate(str.maketrans('','',string.punctuation)).lower().replace(' ', '_')
+            colname = cat.translate(str.maketrans('', '', string.punctuation)).lower().replace(' ', '_')
             df[colname + '_count'] = df.apply(
-                lambda row : row.raw_visit_counts if cat == row.top_category else 0,
-                axis = 1
+                lambda row: row.raw_visit_counts if cat == row.top_category else 0,
+                axis=1
             )
             df[colname + '_daywise'] = df.apply(
-                lambda row : row.visits_by_day if cat == row.top_category else np.zeros(num_days, dtype=int),
-                axis = 1
+                lambda row: row.visits_by_day if cat == row.top_category else np.zeros(num_days, dtype=int),
+                axis=1
             )
 
         grouped = df.groupby('countyFIPS')
@@ -127,34 +120,34 @@ class BaseCountyDataset(Dataset, ABC):
         pass
 
     def read_sg_social_distancing(self, csv_file):
-        df = pd.read_csv(csv_file, 
-                usecols=[
-                        'origin_census_block_group', 
-                        'date_range_start', 
-                        'date_range_end', 
-                        'device_count',
-                        'distance_traveled_from_home',
-                        'completely_home_device_count',
-                        'median_home_dwell_time',
-                        'part_time_work_behavior_devices',
-                        'full_time_work_behavior_devices'
-                    ],
-                dtype={'origin_census_block_group': str}
-            ).set_index('origin_census_block_group')
+        df = pd.read_csv(csv_file,
+                         usecols=[
+                             'origin_census_block_group',
+                             'date_range_start',
+                             'date_range_end',
+                             'device_count',
+                             'distance_traveled_from_home',
+                             'completely_home_device_count',
+                             'median_home_dwell_time',
+                             'part_time_work_behavior_devices',
+                             'full_time_work_behavior_devices'
+                         ],
+                         dtype={'origin_census_block_group': str}
+                         ).set_index('origin_census_block_group')
 
-        #prepare for weighted average
-        df['distance_traveled_from_home']   *= df['device_count']
-        df['median_home_dwell_time']        *= df['device_count']
-        
-        #handle start/end dates as per model
-        
-        df = df.groupby(lambda cbg : str(cbg)[:5]).sum()
+        # prepare for weighted average
+        df['distance_traveled_from_home'] *= df['device_count']
+        df['median_home_dwell_time'] *= df['device_count']
 
-        df['completely_home_device_count']    /= df['device_count']
+        # handle start/end dates as per model
+
+        df = df.groupby(lambda cbg: str(cbg)[:5]).sum()
+
+        df['completely_home_device_count'] /= df['device_count']
         df['part_time_work_behavior_devices'] /= df['device_count']
         df['full_time_work_behavior_devices'] /= df['device_count']
-        df['distance_traveled_from_home']     /= df['device_count']
-        df['median_home_dwell_time']          /= df['device_count']
+        df['distance_traveled_from_home'] /= df['device_count']
+        df['median_home_dwell_time'] /= df['device_count']
 
         df = df.drop(['device_count'], axis=1)
 
@@ -163,14 +156,14 @@ class BaseCountyDataset(Dataset, ABC):
     def read_num_cases(self, start_date: date, end_date: date):
         # Returns the total new cases found between start_date + 1 and end_date
         df = pd.read_csv(config.labels_csv_path, usecols=[
-                'date', 'fips', 'cases'
-            ], dtype={'fips': str}).dropna().set_index('fips')
+            'date', 'fips', 'cases'
+        ], dtype={'fips': str}).dropna().set_index('fips')
 
-        start_date  = start_date.strftime('%Y-%m-%d')
-        end_date    = end_date.strftime('%Y-%m-%d')
+        start_date = start_date.strftime('%Y-%m-%d')
+        end_date = end_date.strftime('%Y-%m-%d')
 
         df_start = df[df['date'] == start_date].drop(['date'], axis=1)
-        df_end   = df[df['date'] == end_date].drop(['date'], axis=1)
+        df_end = df[df['date'] == end_date].drop(['date'], axis=1)
 
         df = df_start.merge(df_end, how='inner', left_index=True, right_index=True, suffixes=('_start', '_end'))
         df['new_cases'] = df['cases_end'] - df['cases_start']
