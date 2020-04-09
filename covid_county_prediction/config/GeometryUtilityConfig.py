ffb446e2c1639e2267768818935a8caae50e27c5
Bora Oztekin <borao@users.noreply.github.com>
2020-04-08 21:42:46 -0500

    Create geometry utility for attaching FIPS to points of interest

---
+++ covid_county_prediction/config/GeometryUtilityConfig.py
@@ -0,0 +1,15 @@
+from covid_county_prediction.config.base_config import Config
+import sys
+import covid_county_prediction.config.global_config as global_config
+import os
+from datetime import timezone, date, timedelta
+from pandas.tseries import offsets
+
+config = Config('GeometryUtilityConfig')
+
+config.core_poi_apr_data_path = os.path.join(global_config.data_base_dir, "CoreApr2020Release-CORE_POI-2020_03-2020-04-07/")
+
+config.svi_data_us_county_data_path = os.path.join(global_config.data_base_dir, "SVI2018_US_COUNTY/")
+
+
+sys.modules[__name__] = config
