39ba95a6c26da086e79995949d8eed41eb332f6b
Ankit Ramchandani <ankit61@tamu.edu>
2020-04-17 22:01:30 -0500

    correcting constructor call

--- covid_county_prediction/CountyWiseTimeDependentFeatures.py
+++ covid_county_prediction/CountyWiseTimeDependentFeatures.py
@@ -2,7 +2,7 @@ from RawFeatures import RawFeatures
 
 class CountyWiseTimeDependentFeatures(RawFeatures):
     def __init__(self, raw_features, feature_name: str):
-        super(CountyWiseTimeDependentFeatures, self).__init__()
+        super(CountyWiseTimeDependentFeatures, self).__init__(raw_features, feature_name)
 
     def extract(self):
         pass
--- covid_county_prediction/TimeDependentFeatures.py
+++ covid_county_prediction/TimeDependentFeatures.py
@@ -2,7 +2,7 @@ from RawFeatures import RawFeatures
 
 class TimeDependentFeatures(RawFeatures):
     def __init__(self, raw_features, feature_name: str):
-        super(TimeDependentFeatures, self).__init__()
+        super(TimeDependentFeatures, self).__init__(raw_features, feature_name)
 
     def extract(self):
         pass
