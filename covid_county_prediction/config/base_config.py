89739632553ab09cea9b3f1c9d054ca45c4e7716
Ankit Ramchandani <ankit61@tamu.edu>
2020-04-22 18:49:07 -0500

    adding set_static_val

--- covid_county_prediction/config/base_config.py
+++ covid_county_prediction/config/base_config.py
@@ -6,14 +6,21 @@ class Config():
         self.description = description
 
     def __setattr__(self, name, value):
-        #inspired by http://code.activestate.com/recipes/65207-constants-in-python/
+        # inspired by http://code.activestate.com/recipes/65207-constants-in-python/
         if name in self.__dict__:
             raise Exception(f'Value of {name} is already set - change original value')
         self.__dict__[name] = value
 
-    def set_static(self, name, func, args):
-        if name in Config.static_members:
+    def set_static(self, name, func, args, overwrite=False):
+        if name in Config.static_members and not overwrite:
             return
 
         Config.static_members[name] = func(args)
+        self.__dict__[name] = Config.static_members[name]
+
+    def set_static_val(self, name, val, overwrite=False):
+        if name in Config.static_members and not overwrite:
+            return
+
+        Config.static_members[name] = val
         self.__dict__[name] = Config.static_members[name]
--- covid_county_prediction/config/global_config.py
+++ covid_county_prediction/config/global_config.py
@@ -5,7 +5,13 @@ from pathlib import Path
 
 config = Config('Global config parameters')
 
-config.data_base_dir = os.path.join(
+data_base_dir = os.path.join(
     Path(os.path.dirname(os.path.abspath(__file__))).parent.parent, 'data')
+data_save_dir = os.path.join(
+    Path(os.path.dirname(os.path.abspath(__file__))).parent.parent, 'saved_covid_data')
+
+config.set_static_val('data_base_dir', data_base_dir)
+config.set_static_val('data_save_dir', data_save_dir)
 
 sys.modules[__name__] = config
+
