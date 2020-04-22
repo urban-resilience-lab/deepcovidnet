0aecbc1e4417941ffd4a97c31801f3bff1bce83a
Ankit Ramchandani <ankit61@tamu.edu>
2020-04-22 18:49:28 -0500

    updating hyperparams

--- covid_county_prediction/config/model_hyperparam_config.py
+++ covid_county_prediction/config/model_hyperparam_config.py
@@ -3,6 +3,9 @@ import sys
 
 config = Config('Hyperparameters for DNN model')
 
-config.embedding_size = 256
+config.embedding_size               = 256
+config.higher_order_features_size   = 256
+
+config.epochs                       = 300
 
 sys.modules[__name__] = config
