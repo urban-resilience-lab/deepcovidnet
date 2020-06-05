c4cab87148c30da0d863c733923f88ee93198cec
Ankit Ramchandani <ankit61@tamu.edu>
2020-06-05 11:15:03 -0500

    combining features with different start dates and intervals

--- covid_county_prediction/CountyWiseTimeDependentFeatures.py
+++ covid_county_prediction/CountyWiseTimeDependentFeatures.py
@@ -3,7 +3,6 @@ from datetime import date, timedelta
 import covid_county_prediction.config.CountyWiseTimeDependentFeaturesConfig as\
     config
 import numpy as np
-from math import ceil
 import torch
 
 
@@ -17,35 +16,58 @@ class CountyWiseTimeDependentFeatures(TimeDependentFeatures):
             raw_features, feature_name, start_date, interval
         )
         self.type = cur_type
-        self.combined_features = [(self.raw_features, self.type)]
+        self.combined_features = [self]
+        self.combined_start_date = self.start_date
 
     def extract_torch_tensor(
         self, county_fips: str, start_date: date, end_date: date
     ):
-        start_idx = self.get_index(start_date)
-        end_idx   = self.get_index(end_date, round_fn=ceil)
+        assert start_date >= self.combined_start_date
 
-        # init tensor of shape (num_type_steps, num_counties, num_features)
+        cur_date = start_date
+        common_dates = []
+        while cur_date < end_date:
+            found_all_features = True
+            for feature_index in range(len(self.combined_features)):
+                if self.combined_features[feature_index].get_index(cur_date) is None:
+                    found_all_features = False
+                    break
+
+            if found_all_features:
+                common_dates.append(cur_date)
+
+            cur_date += timedelta(1)
+
+        assert len(common_dates), 'Features not combinable due to no common dates'
+
+        logging.info(f'Found {len(common_dates)} time steps for combined countywise features')
+
+        # init tensor of shape (num_time_steps, num_counties, num_features)
         tensor = torch.zeros(
-            end_idx - start_idx,
+            len(common_dates),
             self.raw_features[0].shape[0],
             len(self.combined_features)
         )
 
-        for date_index in range(start_idx, end_idx):
+        for i, common_date in enumerate(common_dates):
             for feature_index in range(len(self.combined_features)):
-                df = self.combined_features[feature_index][0][date_index]\
-                        .fillna(0)
-                cur_type = self.combined_features[feature_index][1]
+                date_index = \
+                    self.combined_features[feature_index].get_index(common_date)
+
+                df = self.combined_features[feature_index].raw_features[date_index].fillna(0)
+                cur_type = self.combined_features[feature_index].cur_type
 
                 if cur_type == config.cross_type:
                     features = df.loc[county_fips].to_numpy()
                 elif cur_type == config.const_type:
                     features = np.squeeze(df.to_numpy(), axis=1)
 
-                tensor[date_index - start_idx, :, feature_index] = features
+                tensor[i, :, feature_index] = features
 
         return tensor
 
     def combine(self, other):
-        self.combined_features.append((other.raw_features, other.type))
+        self.combined_start_date = \
+            min(self.combined_start_date, other.start_date)
+
+        self.combined_features.append(other)
--- covid_county_prediction/TimeDependentFeatures.py
+++ covid_county_prediction/TimeDependentFeatures.py
@@ -1,30 +1,40 @@
 from covid_county_prediction.RawFeatures import RawFeatures
 from datetime import date, timedelta
-from math import ceil
 import torch
 
 
 class TimeDependentFeatures(RawFeatures):
-    def __init__(self, raw_features, feature_name: str, start_date: date, interval: timedelta):
+    def __init__(self, raw_features, feature_name: str, start_date: date,
+                 interval: timedelta):
         super(TimeDependentFeatures, self).__init__(raw_features, feature_name)
         self.start_date = start_date
         self.interval   = interval
 
     def get_date(self, i):
+        assert i >= 0 and i < len(self.raw_features)
         return self.start_date + i * self.interval
 
-    def get_index(self, cur_date, round_fn=int):
-        return round_fn((cur_date - self.start_date) / self.interval)
+    def get_index(self, cur_date):
+        does_index_exist = \
+            (cur_date - self.start_date).days % self.interval.days == 0
 
-    def extract_torch_tensor(self, county_fips: str, start_date: date, end_date: date):
-        start_idx   = self.get_index(start_date)
-        end_idx     = self.get_index(end_date, round_fn=ceil)
+        if cur_date < self.start_date or not does_index_exist:
+            return None
+
+        return int((cur_date - self.start_date) / self.interval)
+
+    def extract_torch_tensor(self, county_fips: str, start_date: date,
+                             end_date: date):
+        start_idx = self.get_index(start_date)
+        end_idx   = self.get_index(end_date)
 
         tensor = torch.zeros(end_idx - start_idx, self.raw_features[0].shape[1])
 
         for i in range(start_idx, end_idx):
             if county_fips in self.raw_features[i].index:
-                tensor[i - start_idx] = \
-                    torch.tensor(self.raw_features[i].loc[county_fips].fillna(0).to_numpy())
+                np_tensor = \
+                    self.raw_features[i].loc[county_fips].fillna(0).to_numpy()
+
+                tensor[i - start_idx] = torch.tensor(np_tensor)
 
         return tensor
