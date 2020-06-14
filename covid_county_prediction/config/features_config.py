7cbc190716db8d44d1d819f68d30b2ad4e9eae3e
Ankit Ramchandani <ankit@bora.5vvzfmqtbkcupgeckytcwjjcqa.bx.internal.cloudapp.net>
2020-06-14 19:49:16 +0000

    save features_config county info due to website issues

--- covid_county_prediction/config/features_config.py
+++ covid_county_prediction/config/features_config.py
@@ -1,11 +1,21 @@
 from covid_county_prediction.config.base_config import Config
 import sys
 import pandas as pd
+import os
+import covid_county_prediction.config.global_config as global_config
+
 
 config = Config('general features config parameters')
 
 
+config.counties_save_path = os.path.join(global_config.data_save_dir, 'features_config_counties.csv')
+
+
 def get_county_info(county_info_link):
+    if os.path.exists(config.counties_save_path):
+        df = pd.read_csv(config.counties_save_path).set_index('FIPS')
+        return df
+
     df = pd.read_html(county_info_link)[0].iloc[:-1].set_index('FIPS')
     duplicates = df.loc[df.index.duplicated()].index
     df = df.drop(duplicates)
@@ -24,6 +34,7 @@ def get_county_name_to_iloc(county_df):
     for i in range(county_df.shape[0]):
         ans[county_df.iloc[i].name] = i
     return ans
+
 
 county_info_link = 'https://www.nrcs.usda.gov/wps/portal/nrcs/detail/national/home/?cid=nrcs143_013697'
 
