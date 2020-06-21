9956309f25b8c028e333636ce3aeb7e6beedc6e3
Ankit Ramchandani <ankit@bora.5vvzfmqtbkcupgeckytcwjjcqa.bx.internal.cloudapp.net>
2020-06-21 04:46:53 +0000

    add dropout, remove momentum

--- covid_county_prediction/config/model_hyperparam_config.py
+++ covid_county_prediction/config/model_hyperparam_config.py
@@ -5,11 +5,12 @@ import sys
 def add_hyperparameters(hps):
     with hps(level=HPLevel.HIGH):
         hps.add(name='lr', val=4.46265919891661e-5, hp_range=[0.00001, 1], log_scale=True)
-        hps.add(name='momentum', val=0.9, hp_range=[0.01, 1])
+        # hps.add(name='momentum', val=0.9, hp_range=[0.01, 1])
         hps.add(name='weight_decay', val=0.0001694801, hp_range=[0.00001, 0.01], log_scale=True)
         hps.add(name='batch_size', val=203, hp_range=[16, 512], hp_type=int)
         hps.add(name='embedding_size', val=1789, hp_range=[64, 2048], hp_type=int)
         hps.add(name='higher_order_features_size', val=1346, hp_range=[64, 2048], hp_type=int)
+        hps.add(name='dropout_prob', val=0.5, hp_range=[0, 1])
 
     with hps(level=HPLevel.MEDIUM):
         hps.add(name='lr_decay_factor', val=0.9, hp_range=[0.01, 1])
