21841921a1a4131879ccfe222c90db33eb86a77e
Ankit Ramchandani <ankit@bora.5vvzfmqtbkcupgeckytcwjjcqa.bx.internal.cloudapp.net>
2020-06-22 21:20:38 +0000

    add config for 73.6 val acc

--- covid_county_prediction/config/model_hyperparam_config.py
+++ covid_county_prediction/config/model_hyperparam_config.py
@@ -4,16 +4,16 @@ import sys
 
 def add_hyperparameters(hps):
     with hps(level=HPLevel.HIGH):
-        hps.add(name='lr', val=4.46265919891661e-5, hp_range=[0.00001, 1], log_scale=True)
+        hps.add(name='lr', val=0.000063596, hp_range=[0.000001, 1], log_scale=True)
         # hps.add(name='momentum', val=0.9, hp_range=[0.01, 1])
-        hps.add(name='weight_decay', val=0.0001694801, hp_range=[0.00001, 0.01], log_scale=True)
-        hps.add(name='batch_size', val=203, hp_range=[16, 512], hp_type=int)
-        hps.add(name='embedding_size', val=1789, hp_range=[64, 2048], hp_type=int)
-        hps.add(name='higher_order_features_size', val=1346, hp_range=[64, 2048], hp_type=int)
-        hps.add(name='dropout_prob', val=0.5, hp_range=[0, 1])
+        hps.add(name='weight_decay', val=0.00001, hp_range=[0.000001, 0.01], log_scale=True)
+        hps.add(name='batch_size', val=16, hp_range=[8, 512], hp_type=int)
+        hps.add(name='embedding_size', val=160, hp_range=[64, 2048], hp_type=int)
+        hps.add(name='higher_order_features_size', val=64, hp_range=[64, 2048], hp_type=int)
+        hps.add(name='dropout_prob', val=0.87610, hp_range=[0, 1])
 
     with hps(level=HPLevel.MEDIUM):
-        hps.add(name='lr_decay_factor', val=0.9, hp_range=[0.01, 1])
+        hps.add(name='lr_decay_factor', val=1, hp_range=[0.01, 1])
         hps.add(name='ce_coeff', val=1, hp_range=[0, 100])
 
     with hps(level=HPLevel.LOW):
@@ -29,8 +29,8 @@ def add_hyperparameters(hps):
         )
 
         hps.add(
-            name='past_days_to_consider', val=13, hp_range=[13, 13],
-            hp_type=int, check=(lambda x: x == 13 and (x + 1) % 7 == 0)
+            name='past_days_to_consider', val=27, hp_range=[27, 27],
+            hp_type=int, check=(lambda x: x == 27 and (x + 1) % 7 == 0)
         )
 
 
