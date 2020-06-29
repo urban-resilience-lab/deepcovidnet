3949c723f9aa6e60b76afcb9666d25169aeeb788
Ankit Ramchandani <ankitramchandani61@gmail.com>
2020-06-29 21:12:52 +0000

    add time importance and clean code

--- covid_county_prediction/FeatureAnalyzer.py
+++ covid_county_prediction/FeatureAnalyzer.py
@@ -10,6 +10,7 @@ import covid_county_prediction.config.FeatureAnalyzerConfig as config
 class AnalysisType(Enum):
     GROUP = auto()
     FEATURE = auto()
+    TIME = auto()
 
 
 class FeatureAnalyzer():
@@ -47,19 +48,14 @@ class FeatureAnalyzer():
                                     ).squeeze(-1)
 
                         if batch[k].dim() == 2:
-                            batch[k][:, ftr_idx] = torch.normal(mean=0, std=1, size=orig_vals.shape)
+                            batch[k][:, ftr_idx] = self.randomize_feature(orig_vals.shape)
                         elif batch[k].dim() == 3:
-                            batch[k][:, :, ftr_idx] = torch.normal(mean=0, std=1, size=orig_vals.shape)
+                            batch[k][:, :, ftr_idx] = self.randomize_feature(orig_vals.shape)
                         elif batch[k].dim() == 4:
-                            batch[k][:, :, :, ftr_idx] = torch.normal(mean=0, std=1, size=orig_vals.shape)
+                            batch[k][:, :, :, ftr_idx] = self.randomize_feature(orig_vals.shape)
 
                         # find new acc and keep its track
-                        acc = self._test_batch_and_get_acc(batch)
-                        if ftr_idx in ftr_idx_to_perf:
-                            ftr_idx_to_perf[ftr_idx].update(acc, n=batch[k].shape[0])
-                        else:
-                            ftr_idx_to_perf[ftr_idx] = AverageMeter('')
-                            ftr_idx_to_perf[ftr_idx].update(acc, n=batch[k].shape[0])
+                        self.track_acc(batch, ftr_idx, ftr_idx_to_perf, batch[k].shape[0])
 
                         # restore feature value
                         if batch[k].dim() == 2:
@@ -81,20 +77,41 @@ class FeatureAnalyzer():
             for ftr_idx, k in tqdm(enumerate(keys)):
                 ftr_idx_to_perf = {}
                 for batch in tqdm(self.val_loader):
-                    # randomize feature
-                    batch[k] = torch.normal(mean=0, std=1, size=batch[k].shape)
+                    batch[k] = self.randomize_feature(batch[k].shape)
 
-                    # find new acc and keep its track
-                    acc = self._test_batch_and_get_acc(batch)
-                    if ftr_idx in ftr_idx_to_perf:
-                        ftr_idx_to_perf[ftr_idx].update(acc, n=batch[k].shape[0])
-                    else:
-                        ftr_idx_to_perf[ftr_idx] = AverageMeter('')
-                        ftr_idx_to_perf[ftr_idx].update(acc, n=batch[k].shape[0])
+                    self.track_acc(batch, ftr_idx, ftr_idx_to_perf, batch[k].shape[0])
 
                 # get difference from the best model
                 self.__features.append(
                     [k, self.__orig_acc - ftr_idx_to_perf[ftr_idx].avg]
+                )
+        elif analysis_type == AnalysisType.TIME:
+            timesteps = None
+            time_dep_keys = []
+            for k in keys:
+                if sample_batch[k].dim() == 3:  # time dependent features
+                    time_dep_keys.append(k)
+                    if timesteps is None:
+                        timesteps = sample_batch[k].shape[1]
+                    else:
+                        assert timesteps == sample_batch[k].shape[1]
+
+            for time_idx in tqdm(range(timesteps)):
+                time_idx_to_perf = {}
+                for batch in self.val_loader:
+                    batch_size = None
+                    for k in time_dep_keys:
+                        shape = batch[k][:, time_idx, :].shape
+                        batch[k][:, time_idx, :] = self.randomize_feature(shape)
+                        if batch_size is None:
+                            batch_size = batch[k].shape[0]
+                        else:
+                            assert batch_size == batch[k].shape[0]
+
+                    self.track_acc(batch, time_idx, time_idx_to_perf, batch_size)
+
+                self.__features.append(
+                    [f'day_{time_idx + 1}', self.__orig_acc - time_idx_to_perf[time_idx].avg]
                 )
 
         # rank features
@@ -104,6 +121,17 @@ class FeatureAnalyzer():
         df.to_csv(config.get_ranks_file(self.runner.exp_name), index=False)
 
         return df
+
+    def track_acc(self, batch, ftr_idx, ftr_idx_to_perf, batch_size):
+        acc = self._test_batch_and_get_acc(batch)
+        if ftr_idx in ftr_idx_to_perf:
+            ftr_idx_to_perf[ftr_idx].update(acc, n=batch_size)
+        else:
+            ftr_idx_to_perf[ftr_idx] = AverageMeter('')
+            ftr_idx_to_perf[ftr_idx].update(acc, n=batch_size)
+
+    def randomize_feature(self, shape):
+        return torch.normal(mean=0, std=1, size=shape)
 
     def _test_batch_and_get_acc(self, batch_dict):
         for i in range(len(self.runner.nets)):
