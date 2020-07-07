83d14a27e047220579ebbf7cd8bf2f98cc9cb567
Ankit Ramchandani <ankitramchandani61@gmail.com>
2020-07-07 20:18:19 +0000

    add deep layers, deep intermediate size

--- covid_county_prediction/config/model_hyperparam_config.py
+++ covid_county_prediction/config/model_hyperparam_config.py
@@ -5,19 +5,23 @@ import sys
 
 def add_hyperparameters(hps):
     with hps(level=HPLevel.HIGH):
-        hps.add(name='lr', val=0.00019327, hp_range=[0.000001, 1], log_scale=True)
-        # hps.add(name='momentum', val=0.9, hp_range=[0.01, 1])
-        hps.add(name='weight_decay', val=0.000019119, hp_range=[0.000001, 0.01], log_scale=True)
-        hps.add(name='batch_size', val=85, hp_range=[8, 512], hp_type=int)
-        hps.add(name='embedding_size', val=574, hp_range=[32, 2048], hp_type=int)
-        hps.add(name='higher_order_features_size', val=550, hp_range=[32, 2048], hp_type=int)
-        hps.add(name='dropout_prob', val=0.50884, hp_range=[0, 1])
+        hps.add(name='lr', val=0.00050828, hp_range=[0.000001, 1], log_scale=True)
+        hps.add(name='batch_size', val=128, hp_range=[16, 256], hp_type=int)
+        hps.add(name='embedding_size', val=283, hp_range=[32, 512], hp_type=int)
+        hps.add(name='higher_order_features_size', val=1424, hp_range=[32, 2048], hp_type=int)
+        hps.add(name='deep_intermediate_size', val=512, hp_range=[64, 1024], hp_type=int)
+        hps.add(name='deep_layers', val=4, hp_range=[2, 5], hp_type=int)
+        hps.add(name='dropout_prob', val=0.1, hp_range=[0, 1])
 
     with hps(level=HPLevel.MEDIUM):
-        hps.add(name='lr_decay_factor', val=0.84848, hp_range=[0.01, 1])
-        hps.add(name='ce_coeff', val=16.172, hp_range=[0, 100])
+        pass
 
     with hps(level=HPLevel.LOW):
+        hps.add(name='weight_decay', val=0.0001, hp_range=[0.000001, 0.01], log_scale=True)
+
+        hps.add(name='lr_decay_factor', val=0.6, hp_range=[0.01, 1])
+        hps.add(name='ce_coeff', val=1, hp_range=[0, 100])
+
         hps.add(name='bin_thresh_0', val=0.5, hp_range=[0, 1])
         hps.add(name='bin_thresh_1', val=0.5, hp_range=[0, 1])
         hps.add(name='bin_thresh_2', val=0.5, hp_range=[0, 1])
