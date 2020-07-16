7c8a20cd3cb196a14013d427c52ab20fe1cd8a49
Ankit Ramchandani <ankitramchandani61@gmail.com>
2020-07-16 07:22:42 +0000

    add best config

--- covid_county_prediction/config/model_hyperparam_config.py
+++ covid_county_prediction/config/model_hyperparam_config.py
@@ -5,21 +5,20 @@ import sys
 
 def add_hyperparameters(hps):
     with hps(level=HPLevel.HIGH):
-        hps.add(name='lr', val=0.00041211, hp_range=[0.00001, 0.001], log_scale=True)
-        hps.add(name='weight_decay', val=0.0001, hp_range=[0.000001, 0.1], log_scale=True)
-        hps.add(name='batch_size', val=96, hp_range=[16, 96], hp_type=int)
-        hps.add(name='embedding_size', val=283, hp_range=[32, 512], hp_type=int)
-        hps.add(name='higher_order_features_size', val=1309, hp_range=[32, 1024], hp_type=int)
-        hps.add(name='deep_intermediate_size', val=460, hp_range=[64, 512], hp_type=int)
-        hps.add(name='deep_layers', val=3, hp_range=[2, 6], hp_type=int)
-        hps.add(name='dropout_prob', val=0.5, hp_range=[0, 1])
-        hps.add(name='alpha_dropout_prob', val=0.1, hp_range=[0, 1])
+        hps.add(name='lr', val=0.0002375, hp_range=[0.00001, 0.001], log_scale=True)
+        hps.add(name='weight_decay', val=2.94334e-05, hp_range=[0.000001, 0.1], log_scale=True)
+        hps.add(name='batch_size', val=71, hp_range=[16, 96], hp_type=int)
+        hps.add(name='embedding_size', val=408, hp_range=[32, 512], hp_type=int)
+        hps.add(name='higher_order_features_size', val=362, hp_range=[32, 1024], hp_type=int)
+        hps.add(name='deep_intermediate_size', val=468, hp_range=[64, 512], hp_type=int)
+        hps.add(name='deep_layers', val=2, hp_range=[2, 6], hp_type=int)
+        hps.add(name='dropout_prob', val=0.565068, hp_range=[0, 1])
+        hps.add(name='alpha_dropout_prob', val=0.83584, hp_range=[0, 1])
 
     with hps(level=HPLevel.MEDIUM):
         pass
 
     with hps(level=HPLevel.LOW):
-
         hps.add(name='lr_decay_factor', val=0.6, hp_range=[0.01, 1])
         hps.add(name='ce_coeff', val=1, hp_range=[0, 100])
 
